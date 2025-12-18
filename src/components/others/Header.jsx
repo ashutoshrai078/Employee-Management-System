dbd11ee81feb671e69002be759ad19207a3b48a5
Ashutoshrai078 <ashutoshrai0278@gmail.com>
2025-12-18 17:25:58 +0530

    header issue is remaining

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -12,7 +12,7 @@ const Header = (props) => {
   return (
     <div className='flex items-end justify-between'>
       <h1 className='text-2xl font-medium '>Hello<br/><span className='text-3xl font-semibold'>
-        {props.data.firstName}
+        user
       </span></h1>
       <button onClick={logOutUser} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-lg'>Log Out</button>
     </div>
